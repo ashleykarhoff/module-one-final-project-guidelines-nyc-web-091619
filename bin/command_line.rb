bb01965f14efe9eeb86ef5e3e59334ccc16c66a5
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-01 16:02:21 -0400

    added show horoscope method

--- bin/command_line.rb
+++ bin/command_line.rb
@@ -1,27 +1,29 @@
 class CommandLine
     def greet
         prompt = TTY::Prompt.new
-        name = prompt.ask('What is your name?')
-        puts "Welcome, #{name}!"
-        sign = prompt.select("Choose your zodiac sign:", %w(Aquarius Pisces Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn))
-        puts "You've selected #{sign}"
-
-        User.create(name: name, sign: sign)
-
-        navigate = prompt.select("Would you like to see your horoscope?", %w[yes no])
-
-        if navigate == 'yes'
-            show_horoscope
-        else
-            main_menu
+        result = prompt.collect do 
+            key(:name).ask('What is your name?')
+            
+            key(:sign).select("Choose your zodiac sign:", %w(Aquarius Pisces Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn))
         end
+
+        User.create(name: result[:name], sign: result[:sign])
     end
 
-    # def show_horoscope
-        
-    # end
+    def show_horoscope
+        # find this new user's id
+        user_id = User.last.id
 
-    # # def main_menu
+        # returns user's sign as a string
+        user_sign = User.find_by(id: user_id).sign
 
-    # # end
+        # queries horoscope table for sign's horoscope
+        user_horoscope = Horoscope.all.find do |scope|
+            scope.sign == user_sign
+        end
+
+        # return's user's horoscope
+        puts user_horoscope.horoscope
+    end
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,8 @@
+require 'pry'
+
 require_relative '../config/environment'
 require_relative 'command_line.rb'
 
 new_cli = CommandLine.new 
 new_cli.greet
-
-
-puts "HELLO WORLD"
+new_cli.show_horoscope

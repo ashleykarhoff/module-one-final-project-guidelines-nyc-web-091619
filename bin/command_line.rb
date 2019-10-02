55a66802bae247bed0db0ad9d7ff641e003e24e3
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-02 17:05:14 -0400

    refactored the zodiac sign and greet functions

--- bin/command_line.rb
+++ bin/command_line.rb
@@ -4,6 +4,21 @@ class CommandLine
         TTY::Prompt.new
     end
 
+    def zodiac_sign
+        sign = new_prompt.select("Choose your new zodiac sign:", %w(Aquarius Pisces Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn))
+        puts ""
+        puts "\u{2728}  You've chosen the Zodiac sign, #{sign}. \u{2728}"
+        puts ""
+        sign
+    end
+
+    def change_sign
+        # Updates the user's record with the new zodiac sign
+        # Will call zodiac_sign function
+        User.update(User.last.id, :sign => zodiac_sign)
+        homepage
+    end
+
     def greet
         # Print out a welcome message to the screen
         puts "~ " * 32
@@ -17,49 +32,42 @@ class CommandLine
         puts "~ " * 32
         puts ""
         
-        # Collect user input
-        result = new_prompt.collect do 
-            key(:name).ask('What is your name?')
-            key(:sign).select("Choose your zodiac sign:", %w(Aquarius Pisces Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn))
-        end
-
-        # Create new DB user instance
-        User.create(name: result[:name], sign: result[:sign])
-
+        # Collect user input and create new user instance
+        name = new_prompt.ask('What is your name?')
+        User.create(name: name, sign: zodiac_sign)
     end
 
     def current_user
         User.last
     end
 
-    def show_horoscope
-        # queries horoscope table for sign's horoscope
-        user_horoscope = Horoscope.all.find do |scope|
+    def find_horoscope
+        Horoscope.all.find do |scope|
             scope.sign == current_user.sign
-        end
-
-        # return's user's horoscope
-        puts "Here is today's horoscope for #{current_user.sign}:"
-        puts "~" * 40
-        puts user_horoscope.horoscope
-
-        # ask if they want to add to favorites
-        result = new_prompt.select("Thank you for checking out today's horoscope! /n Would you like to save this horoscope to your favorites?", ['Yes I would!', 'No thanks..'])
-
-        # Saves horoscope to favorites
-        if result == "Yes I would!"
-            Favorites.create(user_id: current_user.id, horoscope_id: user_horoscope.id)
-        else 
-            homepage 
         end
     end
 
-    def change_sign
-        new_sign = new_prompt.select("Choose your new zodiac sign:", %w(Aquarius Pisces Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn))
+    def save_horoscope?
+        # ask if they want to add to favorites
+        result = new_prompt.select("Would you like to save this horoscope to your favorites?", ['Yes I would!', 'No thanks..'])
 
-        user_id = User.last.id
-        User.update(user_id, :sign => new_sign)
-        puts "You've changed your sign to #{new_sign}!"
+        # Saves horoscope to favorites
+        if result == "Yes I would!"
+            Favorites.create(user_id: current_user.id, horoscope_id: find_horoscope.id)
+            puts ""
+            puts "\u{2728}  We've saved this horoscope to your favorites! \u{2728}"
+            puts ""
+        end
+    end
+
+    def show_horoscope
+        # return's user's horoscope
+        puts "Here is today's horoscope for #{current_user.sign}:"
+        puts "~" * 40
+        puts find_horoscope.horoscope
+
+        save_horoscope?
+        homepage
     end
 
     def favorite_horoscopes
@@ -69,19 +77,30 @@ class CommandLine
         # If user hasn't saved any horoscopes, return an error message
         if user_favorites.length == 0
             puts "You haven't saved any favorites yet!"
+            homepage
         end
 
         # Iterate through our user's favorites and query the horoscope table
+        puts "\u{2728}  Here are your favorite horoscopes \u{2728}"
+        puts "~ " * 35 
+
         user_favorites.each do |favorite|
             horoscopes = Horoscope.where(id: favorite.horoscope_id)
 
             # Print out each horoscope
             horoscopes.each do |scope|
-                puts scope.horoscope
+                puts "\u{1F52E}  " + scope.horoscope
                 puts ""
-                puts "~" * 25
+                puts "~ " * 25
+                puts ""
             end
         end
+
+        homepage
+    end
+
+    def sign_info
+
     end
 
     def homepage
--- bin/run.rb
+++ bin/run.rb
@@ -4,10 +4,10 @@ require_relative '../config/environment'
 require_relative 'command_line.rb'
 
 new_cli = CommandLine.new 
-new_cli.greet
-new_cli.show_horoscope
-new_cli.homepage
+# new_cli.greet
+# new_cli.show_horoscope
+# new_cli.homepage
 
-# binding.pry
+binding.pry
 
 #new_cli.change_sign
